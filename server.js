94c63d74ddf924c864bc842fb374fce541ed3fac
triyanarief <triyanarief@icloud.com>
2017-02-15 19:06:42 +0700

    update validasi username dan istyping

--- server.js
+++ server.js
@@ -1,25 +1,56 @@
-var app = require('express')();
+var app  = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io   = require('socket.io')(http);
 
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname +'/index.html');
+var users = {};
+var usernames = [];
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
-  // kalau ada chat baru
-  socket.on('newMessages', function(msg) {
-    io.emit('newMessages', msg);
-    console.log('Chat baru : ' + msg);
-  });
+io.on('connection', function(socket){
 
-  // kalau ada yang disconnect
-  socket.on('disconnect', function(msg) {
-    console.log('User disconnected');
-  });
+    // respon kalau ada user baru masuk
+    socket.broadcast.emit('newMessage', 'Someone Connected');
+
+    // ketika ada user daftar
+    socket.on('registerUser', function(username){
+      if(usernames.indexOf(username) != -1) {
+        socket.emit('registerRespond', false);
+      } else {
+        users[socket.id] = username;
+        usernames.push(username);
+        socket.emit('registerRespond', true);
+        io.emit('addOnlineUsers', usernames);
+      }
+    })
+
+    //kalo ada message baru
+    socket.on('newMessage', function(msg){
+      io.emit('newMessage', msg);
+      console.log('Chat baru: ' + msg);
+    });
+
+    //kalo ada message baru
+    socket.on('newTyping', function(msg){
+      io.emit('newTyping', msg);
+    });
+
+    //kalo user disconnect
+    socket.on('disconnect', function(msg){
+      socket.broadcast.emit('newMessage', 'Someone left the chat');
+
+      var index = usernames.indexOf(users[socket.id]);
+      usernames.splice(index, 1);
+      delete users[socket.id];
+
+      io.emit('addOnlineUsers', usernames);
+    });
+
 });
 
-http.listen(3000, function() {
-  console.log('listen on 3000 ...');
+http.listen(3000, function(){
+  console.log('listening on 3000...');
 });
